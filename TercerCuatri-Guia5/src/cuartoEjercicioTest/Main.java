2eb8c3b5bec171af407993c71c8f4534ad59c730
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-05 15:30:08 -0300

    Clase Main
    (Terminar)

---
+++ TercerCuatri-Guia5/src/cuartoEjercicioTest/Main.java
@@ -0,0 +1,13 @@
+package cuartoEjercicioTest;
+
+public class Main {
+
+    /*Crea una clase base llamada Numero y dos subclases llamadas Entero y Decimal. Agrega un método llamado
+    imprimir() a la clase Numero y redefine este método en las subclases para que imprima el número correspondiente.
+    Luego, crea clase conjunto con un set de números y una lista de números y llena ambas estructuras con objetos de
+    las subclases. Después, crea un método que tome un set de números y un objeto de la lista de números y haga que
+    cada número en el set y en la lista imprima su valor.*/
+
+
+
+}
