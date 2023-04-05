157cb08eaaa6769f22ff166adb410827b92ecff7
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-05 15:29:07 -0300

    Clase Decimal (extends Numero)
    Atributo: valor
    Métodos: constructores, set, get, imprimirNumero

---
+++ TercerCuatri-Guia5/src/cuartoEjercicio/Decimal.java
@@ -0,0 +1,26 @@
+package cuartoEjercicio;
+
+public class Decimal extends Numero{
+
+    private double valor;
+
+    public Decimal() {
+    }
+
+    public Decimal(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public void imprimirNumero() {
+        this.getValor();
+    }
+}
