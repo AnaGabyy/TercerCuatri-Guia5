c51c7055612646541bcf7591965c1a3ffdeea0c6
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-05 14:38:18 -0300

    Clase Main
    Prueba de objetos

---
+++ TercerCuatri-Guia5/src/segundoEjercicioTest/Main.java
@@ -0,0 +1,33 @@
+package segundoEjercicioTest;
+
+import segundoEjercicio.Fruta;
+import segundoEjercicio.Frutero;
+import segundoEjercicio.Manzana;
+import segundoEjercicio.Naranja;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*Crea una clase base llamada Fruta y dos subclases llamadas Manzana y Naranja. Agrega un método llamado
+        obtenerColor() a la clase Fruta y redefine este método en las subclases para que las manzanas sean rojas
+        y las naranjas sean naranjas. Luego, crea una clase frutero con un método que tome una lista de frutas y
+        devuelva una lista con los colores de las frutas.*/
+
+        Manzana manzana = new Manzana();
+        Naranja naranja = new Naranja();
+
+        List<Fruta> listaFrutas = new ArrayList<Fruta>();
+        listaFrutas.add(manzana);
+        listaFrutas.add(naranja);
+
+        Frutero frutero = new Frutero(listaFrutas);
+
+        frutero.mostrarColorFrutas();
+
+
+    }
+}
