e554e56c9091b669b8e1611990d82a339462e2f7
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-05 15:29:54 -0300

    Clase Entero (extends Numero)
    Atributo: valor
    Métodos: constructores, get, set, imprimirNumero

---
+++ TercerCuatri-Guia5/src/cuartoEjercicio/Entero.java
@@ -0,0 +1,26 @@
+package cuartoEjercicio;
+
+public class Entero extends Numero{
+
+    private int valor;
+
+    public Entero(){
+    }
+
+    public Entero(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public void imprimirNumero() {
+        this.getValor();
+    }
+}
