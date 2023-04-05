403b76d003cc9077b146b306dceeb8a902b3ddea
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-05 14:35:43 -0300

    Clase abstracta Fruta
    Con atributo color y métodos (constructor, getters y setters y obtenerSonido (abstracto))

---
+++ TercerCuatri-Guia5/src/segundoEjercicio/Fruta.java
@@ -0,0 +1,21 @@
+package segundoEjercicio;
+
+public abstract class Fruta {
+
+    private String color;
+
+    public Fruta(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract String obtenerSonido();
+
+}
