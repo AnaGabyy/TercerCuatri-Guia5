43a00df3b43080b71561f981d2ce0aa8c64b0b22
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-05 14:37:28 -0300

    Clase Frutero
    Atributo (lista de frutas) y métodos (constructor, gtters y setters y mostrarColorFrutas (recorro la lista con foreach))

---
+++ TercerCuatri-Guia5/src/segundoEjercicio/Frutero.java
@@ -0,0 +1,28 @@
+package segundoEjercicio;
+
+import java.util.List;
+
+public class Frutero {
+
+    private List<Fruta> listaFrutas;
+
+    public Frutero(List<Fruta> listaFrutas) {
+        this.listaFrutas = listaFrutas;
+    }
+
+    public List<Fruta> getListaFrutas() {
+        return listaFrutas;
+    }
+
+    public void setListaFrutas(List<Fruta> listaFrutas) {
+        this.listaFrutas = listaFrutas;
+    }
+
+    public void mostrarColorFrutas(){
+
+        for (Fruta frutas : listaFrutas) {
+            System.out.println(frutas.getColor());
+        }
+    }
+
+}
