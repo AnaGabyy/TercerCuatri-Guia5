f4e7164469fb65ab7f34f8023d0c81310088a2c7
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-10 10:45:53 -0300

    Clase Conjunto
    Atributos: Set y List de Numeros (enteros y decimales)
    Métodos: constructor, setters y getters, e imprimirSetYLista (muestra el contenido del set y la lista recoriendolos con foreach)

--- TercerCuatri-Guia5/src/cuartoEjercicio/Conjunto.java
+++ TercerCuatri-Guia5/src/cuartoEjercicio/Conjunto.java
@@ -1,4 +1,51 @@
 package cuartoEjercicio;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Conjunto {
+
+    private Set<Numero> setDeNumerosEnteros;
+    private List<Numero> listDeNumerosDecimales;
+
+    public Conjunto() {
+        this.listDeNumerosDecimales = new ArrayList<>();
+        this.setDeNumerosEnteros = new HashSet<>();
+    }
+
+    public Set<Numero> getSetDeNumerosEnteros() {
+        return setDeNumerosEnteros;
+    }
+
+    public void setSetDeNumerosEnteros(Set<Numero> setDeNumerosEnteros) {
+        this.setDeNumerosEnteros = setDeNumerosEnteros;
+    }
+
+    public List<Numero> getListDeNumerosDecimales() {
+        return listDeNumerosDecimales;
+    }
+
+    public void setListDeNumerosDecimales(List<Numero> listaDeNumerosDecimales) {
+        this.listDeNumerosDecimales = listaDeNumerosDecimales;
+    }
+
+
+    public void imprimirSetYLista(){
+
+        System.out.println("Números enteros: ");
+
+        for (Numero numerosEnteros : setDeNumerosEnteros) {
+            numerosEnteros.imprimirNumero();
+        }
+
+        System.out.println("Números decimales: ");
+
+        for (Numero numerosDecimales : listDeNumerosDecimales) {
+            numerosDecimales.imprimirNumero();
+        }
+    }
+
+
 }
